fe71c69e12323226bb4f0ca9f5f82dd75f87f841
Tomsaxmuzik🎷 <Pessimatommy03@gmail.com>
2024-11-07 02:36:32 +0000

    Switch Case

---
+++ 2:Conditional_Statement/switch_Statement.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Switch Statement</title>
+</head>
+<body>
+    <?php
+          $a = 10;
+          $b = 5;
+          $c = 9;
+
+          switch(true){
+            case $a > $b;
+            echo "<h1>Tommy is a Genius</h1>";
+            break;
+
+            default:
+            echo "<h1>Tommy is the greatest</h1>";
+          }
+    ?>
+</body>
+</html>
